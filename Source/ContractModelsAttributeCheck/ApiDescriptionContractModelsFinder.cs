d551ddf0336f52d4169c6d69ceea6856ef40c346
darthfabar <darth-fabar@posteo.de>
2021-04-02 22:34:23 +0200

    [ADD] ApiDescriptionContractModelsFinder

---
+++ Source/ContractModelsAttributeCheck/ApiDescriptionContractModelsFinder.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContractModelsAttributeCheck
+{
+    /// <summary>
+    /// Enables you to get all used types in your controllers
+    /// </summary>
+    public class ApiDescriptionContractModelsFinder
+    {
+        /// <summary>
+        /// get a IApiDescriptionGroupCollectionProvider from your dependency injection and use it to query all used types
+        /// </summary>
+        /// <param name="apiDescription"></param>
+        /// <param name="mediaTypesToCheck"></param>
+        /// <returns></returns>
+        public DistinctTypeList GetUsedContractTypes(IApiDescriptionGroupCollectionProvider apiDescription, string? mediaTypesToCheck = null)
+        {
+            var apiDescriptions = apiDescription.ApiDescriptionGroups?.Items?
+                .SelectMany(s => s.Items)
+                .Where(w => w is not null)
+                ;
+            var usedTypes = new DistinctTypeList();
+            foreach (var description in apiDescriptions!)
+            {
+                var bodyParameter = description.ParameterDescriptions.Where(IsBodyParameter);
+                usedTypes.AddRange(bodyParameter.Select(s => s.Type));
+
+                var responseParameter = description.SupportedResponseTypes.Where(w => w.Type != null && IsCorrectMediaType(mediaTypesToCheck, w));
+                usedTypes.AddRange(responseParameter.Select(s => s.Type));
+            }
+
+            return usedTypes;
+        }
+
+        private bool IsBodyParameter(ApiParameterDescription w)
+        {
+            return w.Source.IsFromRequest && string.Equals(w.Source.DisplayName, "body", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private static bool IsCorrectMediaType(string? mediaTypesToCheck, ApiResponseType w)
+        {
+            return string.IsNullOrEmpty(mediaTypesToCheck) || w.ApiResponseFormats.Select(s => s.MediaType).Contains(mediaTypesToCheck);
+        }
+    }
+}
