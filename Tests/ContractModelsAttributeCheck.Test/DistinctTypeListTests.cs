9cb756eb68b4db1c0ee1b2841f7ef7ce2d61661c
darthfabar <darth-fabar@posteo.de>
2021-04-04 20:39:24 +0200

    [ADD] tests for distincttypelist

--- Tests/ContractModelsAttributeCheck.Test/DistinctTypeListTests.cs
+++ Tests/ContractModelsAttributeCheck.Test/DistinctTypeListTests.cs
@@ -1,3 +1,4 @@
+using ContractModelsAttributeCheck.Test.TestTypes;
 using FluentAssertions;
 using System.Linq;
 using Xunit;
@@ -14,6 +15,30 @@ namespace ContractModelsAttributeCheck.Test
             distinctTypes.Add(typeof(DistinctTypeList));
 
             distinctTypes.GetAllTypes().Count().Should().Be(1);
+        }
+
+        [Fact]
+        public void New_DistinctTypeList_Should_Be_Empty()
+        {
+            var distinctTypes = new DistinctTypeList();
+            distinctTypes.IsEmpty.Should().BeTrue();
+        }
+
+        [Fact]
+        public void AddRange_Should_Contain_Types_Of_Both_Lists()
+        {
+            var typeList1 = new[] { typeof(TestClassWithAttributes), typeof(TestClassWithAttributes.ClassA) };
+            var typeList2 = new[] { typeof(TestClassWithoutAttributes), typeof(TestClassWithoutAttributes.ClassA) };
+
+            var distinctList1 = new DistinctTypeList();
+            var distinctList2 = new DistinctTypeList();
+
+            distinctList1.AddRange(typeList1);
+            distinctList2.AddRange(typeList2);
+
+            distinctList1.AddRange(distinctList2);
+
+            distinctList1.GetAllTypes().Count().Should().Be(typeList1.Length + typeList2.Length);
         }
     }
 }
